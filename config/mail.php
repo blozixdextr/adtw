3088a98cc870192ce4e39be4576ad7d61d60a4e8
ifrond <info@ifrond.com>
2015-09-10 11:16:35 +0200

    rename default sender name

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => 'robot@adtw.ch', 'name' => 'Mailer'],
+    'from' => ['address' => 'robot@adtw.ch', 'name' => 'Adtw.ch'],
 
     /*
     |--------------------------------------------------------------------------
