b693c1ce6c11d799bf2aaf033a61fc2884fbe7d5
blozixdextr <blozixdextr@users.noreply.github.com>
2015-09-11 13:19:14 +0200

    update

--- resources/views/app/includes/head/menu/client/left.blade.php
+++ resources/views/app/includes/head/menu/client/left.blade.php
@@ -1,14 +1,14 @@
 <div class="nav-column">
-    <h1><i class="fa fa-cogs"></i> Settings</h1>
+    <h1>Dashboard</h1>
     <div class="nav-settings">
-        <p class="nav-column-item"><a href="/user/client">Dashboard</a></p>
-        <p class="nav-column-item"><a href="/user/client/search">Search</a></p>
-        <h4 class="nav-column-title">Account</h4>
+        <p class="nav-column-item"><a href="/user/client">Timeline</a></p>
+        <p class="nav-column-item"><a href="/user/client/search">Search & Buy</a></p>
         <p class="nav-column-item"><a href="/user/client/ads">My Ads</a></p>
-        <p class="nav-column-item"><a href="/user/client/billing">Billing</a></p>
+
     </div>
     <div class="nav-user">
         <h4 class="nav-column-title">User</h4>
+        <p class="nav-column-item"><a href="/user/client/billing">Billing</a></p>
         <p class="nav-column-item"><a href="/user/client/profile">My Profile</a></p>
         <p class="nav-column-item"><a href="/user/client/notification">Notifications</a></p>
         <p class="nav-column-item"><a href="/auth/logout">Logout</a></p>
