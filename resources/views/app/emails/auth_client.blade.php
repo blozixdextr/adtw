881a772f4c4201a5480aadef4bbced13327ae6e6
junkwin <s.studzinskiy@yandex.ru>
2015-09-14 20:11:37 +0300

    Update auth_client.blade.php
    Корректировка. Попытка #6

--- resources/views/app/emails/auth_client.blade.php
+++ resources/views/app/emails/auth_client.blade.php
@@ -98,7 +98,7 @@
                                     <td align="center" valign="top">
                                         <table border="0" cellpadding="0" cellspacing="0" width="500" class="flexibleContainer">
                                             <tr>
-                                                <td align="center" valign="top" width="500" class="flexibleContainerCell">
+                                                <td align="center" style="margin-top:25px;" valign="top" width="500" class="flexibleContainerCell">
                                                     <table border="0" cellpadding="10" cellspacing="0" width="100%">
                                                         <tr>
                                                             <td align="center" valign="top" class="textContent">
