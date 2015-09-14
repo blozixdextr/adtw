7b41bcb977cfb78a94d3d648682e3d05010fa685
junkwin <s.studzinskiy@yandex.ru>
2015-09-14 23:15:14 +0300

    Update footer.blade.php
    Корректировал футер #4

--- resources/views/app/includes/footer.blade.php
+++ resources/views/app/includes/footer.blade.php
@@ -1,8 +1,8 @@
 <footer id="footer">
     <ul class="icons">
-        <li><a href="#"><i class="fa fa-facebook"></i><span> Facebook</span></a></li>
-        <li><a href="#"><i class="fa fa-twitter"></i><span> Twitter</span></a></li>
-        <li><a href="#"><i class="fa fa-envelope-o"></i><span> Email</span></a></li>
+        <li><a href="#"><i class="fa fa-facebook"></i> <span> Facebook</span></a></li>
+        <li><a href="#"><i class="fa fa-twitter"></i> <span> Twitter</span></a></li>
+        <li><a href="#"><i class="fa fa-envelope-o"></i> <span> Email</span></a></li>
     </ul>
     <ul class="copyright">
         <li>&copy; www.adtw.ch</li>
