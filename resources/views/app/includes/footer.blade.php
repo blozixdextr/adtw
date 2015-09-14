b2c7f4c8f2041aa39f69618326302e7d12460e1c
junkwin <s.studzinskiy@yandex.ru>
2015-09-14 23:11:01 +0300

    Update footer.blade.php
    Убрал лишние классы в футере

--- resources/views/app/includes/footer.blade.php
+++ resources/views/app/includes/footer.blade.php
@@ -1,8 +1,8 @@
 <footer id="footer">
     <ul class="icons">
-        <li><a href="#" class="fa fa-facebook"><span class="label"> Facebook</span></a></li>
-        <li><a href="#" class="fa fa-twitter"><span class="label"> Twitter</span></a></li>
-        <li><a href="#" class="fa fa-envelope-o"><span class="label"> Email</span></a></li>
+        <li><a href="#" class="fa fa-facebook"><span> Facebook</span></a></li>
+        <li><a href="#" class="fa fa-twitter"><span> Twitter</span></a></li>
+        <li><a href="#" class="fa fa-envelope-o"><span> Email</span></a></li>
     </ul>
     <ul class="copyright">
         <li>&copy; www.adtw.ch</li>
