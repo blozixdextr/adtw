93fa67d087699473fd1cc58dd02dee63ae612f50
junkwin <s.studzinskiy@yandex.ru>
2015-09-15 20:36:01 +0300

    Update top.blade.php
    Корректировка хедера

--- resources/views/app/includes/head/menu/guest/top.blade.php
+++ resources/views/app/includes/head/menu/guest/top.blade.php
@@ -1,6 +1,6 @@
 <header id="header" class="text-center">
     <div class="shadow-fon">
-        <a href=""><img src="/assets/app/images/gold.gif"></a>
+        <a href=""><img src="/assets/app/images/gold.gif"></a><br/>
         <a href="" class="big-logo-link"></a><br>
         <span class="title-intro">Ad exchange for<br></span>
         <span class="title-descr">streamers & adverisers</span>
