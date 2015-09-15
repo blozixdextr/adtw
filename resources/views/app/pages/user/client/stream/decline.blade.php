9c78e9fe1628a7ba97ba0c05b0573fe5a6a66073
junkwin <s.studzinskiy@yandex.ru>
2015-09-15 21:27:28 +0300

    Update decline.blade.php
    Поправлял кнопки

--- resources/views/app/pages/user/client/stream/decline.blade.php
+++ resources/views/app/pages/user/client/stream/decline.blade.php
@@ -19,7 +19,7 @@
 
     <div class="form-group">
         <div class="col-sm-offset-3 col-sm-9">
-            <button type="submit" class="btn-violet">Save</button>
+            <button type="submit" class="btn-white">Save</button>
         </div>
     </div>
 
