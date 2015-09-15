5b49e084e44eb8cc41d8fbd64f1681cff4416052
junkwin <s.studzinskiy@yandex.ru>
2015-09-15 15:39:06 +0300

    Update top.blade.php
    Оборачиваю все меню в обычный блок div для того тчобы центрировать его и дать ему ширину

--- resources/views/app/includes/head/menu/twitcher/top.blade.php
+++ resources/views/app/includes/head/menu/twitcher/top.blade.php
@@ -1,22 +1,24 @@
 <nav class="second-nav">
-    <header id="header">
-        <a href=""><img src="/assets/app/images/gold.gif"></a>
-        <hgroup>
-            <h1><a href="/user/twitcher">Adtw.ch</a></h1>
-            <h2>Balance: $ {{ number_format($user->balance, 2) }}</h2>
-        </hgroup>
-    </header>
-    <ul class="head-nav">
-        <li><a class="btn-violet" href="/user/twitcher">Dashboard</a></li>
-        <li><a class="btn-violet" href="/user/twitcher/ads">My Ads</a></li>
-        <li><a class="btn-violet" href="/user/twitcher/billing">Billing</a></li>
-        <li><a class="btn-violet" href="/faq">FAQ</a></li>
-        <li class="menu-hidden" ><h1>Settings</h1></li>
-        <li class="menu-hidden" ><h2>Account</h2></li>
-        <li class="menu-hidden" ><a class="btn-violet" href="/user/twitcher/billing">Billing</a></li>
-        <li class="menu-hidden" ><h2>User</h2></li>
-        <li class="menu-hidden" ><a class="btn-violet" href="/user/twitcher/profile">Profile</a></li>
-        <li class="menu-hidden" ><a class="btn-violet" href="/user/twitcher/notification">Notifications</a></li>
-    </ul>
-    <div class="handle">Menu <span>&#9776;</span></div>
+    <div>
+        <header id="header">
+            <a href=""><img src="/assets/app/images/gold.gif"></a>
+            <hgroup>
+                <h1><a href="/user/twitcher">Adtw.ch</a></h1>
+                <h2>Balance: $ {{ number_format($user->balance, 2) }}</h2>
+            </hgroup>
+        </header>
+        <ul class="head-nav">
+            <li><a class="btn-violet" href="/user/twitcher">Dashboard</a></li>
+            <li><a class="btn-violet" href="/user/twitcher/ads">My Ads</a></li>
+            <li><a class="btn-violet" href="/user/twitcher/billing">Billing</a></li>
+            <li><a class="btn-violet" href="/faq">FAQ</a></li>
+            <li class="menu-hidden" ><h1>Settings</h1></li>
+            <li class="menu-hidden" ><h2>Account</h2></li>
+            <li class="menu-hidden" ><a class="btn-violet" href="/user/twitcher/billing">Billing</a></li>
+            <li class="menu-hidden" ><h2>User</h2></li>
+            <li class="menu-hidden" ><a class="btn-violet" href="/user/twitcher/profile">Profile</a></li>
+            <li class="menu-hidden" ><a class="btn-violet" href="/user/twitcher/notification">Notifications</a></li>
+        </ul>
+        <div class="handle">Menu <span>&#9776;</span></div>
+    </div>
 </nav>
