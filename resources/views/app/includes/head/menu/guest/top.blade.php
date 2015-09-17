7c686408ebe1fa7aeb47349e5aa2c68dffe9d004
junkwin <s.studzinskiy@yandex.ru>
2015-09-17 22:34:50 +0300

    Update top.blade.php

--- resources/views/app/includes/head/menu/guest/top.blade.php
+++ resources/views/app/includes/head/menu/guest/top.blade.php
@@ -1,10 +1,10 @@
 <header id="header" class="text-center">
-    <!--<div class="shadow-fon"></div>-->
+    <div class="shadow-fon">
         <a href=""><img src="/assets/app/images/gold.gif"></a><br/>
         <a href="" class="big-logo-link"></a><br>
         <span class="title-intro">Ad exchange for<br></span>
         <span class="title-descr">streamers & adverisers</span>
-    
+    </div>
 </header>
 <!--<nav class="main-nav">
     <ul class="head-nav">
