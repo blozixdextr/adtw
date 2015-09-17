3ce49d44ba7af68b1befa31cdf5b2fbe3a0b6fa8
junkwin <s.studzinskiy@yandex.ru>
2015-09-17 16:02:45 +0300

    Update footer.blade.php

--- resources/views/app/includes/footer.blade.php
+++ resources/views/app/includes/footer.blade.php
@@ -1,10 +1,12 @@
 <footer id="footer">
-    <ul class="icons">
-        <li><a href="#"><i class="fa fa-facebook"></i> <span> Facebook</span></a></li>
-        <li><a href="#"><i class="fa fa-twitter"></i> <span> Twitter</span></a></li>
-        <li><a href="#"><i class="fa fa-envelope-o"></i> <span> Email</span></a></li>
-    </ul>
-    <ul class="copyright">
-        <li>&copy; www.adtw.ch</li>
-    </ul>
+    <div class="shadow-fon">
+        <ul class="icons">
+            <li><a href="#"><i class="fa fa-facebook"></i> <span> Facebook</span></a></li>
+            <li><a href="#"><i class="fa fa-twitter"></i> <span> Twitter</span></a></li>
+            <li><a href="#"><i class="fa fa-envelope-o"></i> <span> Email</span></a></li>
+        </ul>
+        <ul class="copyright">
+            <li>&copy; www.adtw.ch</li>
+        </ul>
+    </div>
 </footer>
