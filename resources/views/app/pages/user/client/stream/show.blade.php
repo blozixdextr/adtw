0db919aa017254c3ce6f7a22bc4a3385d129a802
junkwin <s.studzinskiy@yandex.ru>
2015-09-19 11:57:41 +0300

    Update show.blade.php

--- resources/views/app/pages/user/client/stream/show.blade.php
+++ resources/views/app/pages/user/client/stream/show.blade.php
@@ -66,7 +66,7 @@
                     <td class="success-live">
                         @if ($t->status == 'live' && $t->screenshot)
                             <a href="{{ $t->screenshot }}">{{ $t->status }}</a>
-                            <div class="streamer-online-view"></div>
+                            <div class="streamer-online-view"><i class="fa fa-eye"></i></div>
                         @else
                             <em>{{ $t->status }}</em>
                         @endif
