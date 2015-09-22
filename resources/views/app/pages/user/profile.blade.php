415d33ad2bdb40c77c2d9a146c388dee5553d22d
junkwin <s.studzinskiy@yandex.ru>
2015-09-22 14:28:15 +0300

    Update profile.blade.php

--- resources/views/app/pages/user/profile.blade.php
+++ resources/views/app/pages/user/profile.blade.php
@@ -7,17 +7,19 @@
     <link rel="stylesheet" href="/assets/app/css/libs/flags/css/flag-icon.min.css">
 @endsection
 @section('content')
-    <h1>User's Profile</h1>
-    <h2>{{ $userView->name }}  <small>/ {{ $userView->type }}</small></h2>
-    <p>Registered: {{ $userView->created_at->format('Y-m-d') }}</p>
-    <p>Last visit: {{ $userView->last_activity->format('Y-m-d') }}</p>
-    @if ($userView->language_id)
-        <p>Language: {{ $userView->language->title }}</p>
-    @endif
-    @if ($userView->type == 'twitcher')
-        <p>{{ $userView->twitch_followers }} followers</p>
-        <p>{{ $userView->twitch_videos }} videos</p>
-        <p>{{ $userView->twitch_views }} views</p>
-        <a href="/user/client/banner/{{ $userView->id }}" class="work-button">Order banner</a>
-    @endif
+    <div class="streamer-page">
+        <h1>User's Profile</h1>
+        <h2>{{ $userView->name }}  <small>/ {{ $userView->type }}</small></h2>
+        <p>Registered: {{ $userView->created_at->format('Y-m-d') }}</p>
+        <p>Last visit: {{ $userView->last_activity->format('Y-m-d') }}</p>
+        @if ($userView->language_id)
+            <p>Language: {{ $userView->language->title }}</p>
+        @endif
+        @if ($userView->type == 'twitcher')
+            <p>{{ $userView->twitch_followers }} followers</p>
+            <p>{{ $userView->twitch_videos }} videos</p>
+            <p>{{ $userView->twitch_views }} views</p>
+            <a href="/user/client/banner/{{ $userView->id }}" class="work-button">Order banner</a>
+        @endif
+    </div>
 @endsection
