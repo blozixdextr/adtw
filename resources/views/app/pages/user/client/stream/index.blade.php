65c0ec0e8782de81ef1f5045de3c6532cd1a87c8
junkwin <s.studzinskiy@yandex.ru>
2015-09-22 20:00:09 +0300

    Update index.blade.php

--- resources/views/app/pages/user/client/stream/index.blade.php
+++ resources/views/app/pages/user/client/stream/index.blade.php
@@ -22,7 +22,56 @@
 @endsection
 
 @section('content')
-
+    <div class="booking-table">
+        <div class="booking-table-row booking-name">
+            <div class="booking-ban-size">
+                <p>Banner</p>
+            </div>
+            <div class="booking-streamer">
+                <p>Streamer</p>
+            </div>
+            <div class="booking-cost-limit">
+                <p>Cost limit</p>
+            </div>
+            <div class="booking-actions">
+                <p>Actions</p>
+            </div>
+            <div class="booking-actions">
+                <p>Actions</p>
+            </div>
+        </div>
+        <div class="booking-table-row booking-name">
+            <div class="booking-ban-size">
+                <p>Banner</p>
+            </div>
+            <div class="booking-streamer">
+                <p>Streamer</p>
+            </div>
+            <div class="booking-cost-limit">
+                <p>Cost limit</p>
+            </div>
+            <div class="booking-actions">
+                <p>Actions</p>
+            </div>
+            <div class="booking-actions">
+                <p>Actions</p>
+            </div>
+        </div>
+        <div class="booking-table-row booking-name">
+            <div class="booking-ban-size">
+                <p>Banner</p>
+            </div>
+            <div class="booking-streamer">
+                <p>Streamer</p>
+            </div>
+            <div class="booking-cost-limit">
+                <p>Cost limit</p>
+            </div>
+            <div class="booking-actions">
+                <p>Actions</p>
+            </div>
+        </div>
+    </div>
     <h1>My Orders</h1>
     <div class="booking-table">
         <div class="booking-table-row booking-name">
