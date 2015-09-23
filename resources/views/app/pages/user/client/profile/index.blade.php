18eddfcb04dfc0eaf2999adc59afe8854a93de3f
junkwin <s.studzinskiy@yandex.ru>
2015-09-24 00:54:50 +0300

    Update index.blade.php

--- resources/views/app/pages/user/client/profile/index.blade.php
+++ resources/views/app/pages/user/client/profile/index.blade.php
@@ -4,7 +4,7 @@
     <h1><i class="fa fa-user"></i> Profile</h1>
     {!! Form::open(['url' => '/user/client/profile/save', 'class' => 'form-horizontal']) !!}
 
-    <div class="form-group-1 {!! ($errors && $errors->has('first_name')) ? ' has-error' : '' !!}">
+    <div class="form-group-1 width-50 {!! ($errors && $errors->has('first_name')) ? ' has-error' : '' !!}">
         {!! Form::label('first_name', 'First Name', ['class' => 'control-label']) !!}
         <div>
             {!! Form::text('first_name', old('first_name', $profile->first_name), ['class' => 'form-control', 'placeholder' => 'First Name', 'required' => 'required']) !!}
@@ -12,7 +12,7 @@
         </div>
     </div>
 
-    <div class="form-group-1 {!! ($errors && $errors->has('last_name')) ? ' has-error' : '' !!}">
+    <div class="form-group-1 width-50 {!! ($errors && $errors->has('last_name')) ? ' has-error' : '' !!}">
         {!! Form::label('last_name', 'Last Name', ['class' => 'control-label']) !!}
         <div>
             {!! Form::text('last_name', old('last_name', $profile->last_name), ['class' => 'form-control', 'placeholder' => 'Last Name', 'required' => 'required']) !!}
