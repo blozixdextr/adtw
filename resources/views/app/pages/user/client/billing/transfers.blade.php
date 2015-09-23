e6f18c4e5cbd160ed08d291e690b426f0491c33a
junkwin <s.studzinskiy@yandex.ru>
2015-09-23 17:17:05 +0300

    Update transfers.blade.php

--- resources/views/app/pages/user/client/billing/transfers.blade.php
+++ resources/views/app/pages/user/client/billing/transfers.blade.php
@@ -1,10 +1,10 @@
 @extends('app.layouts.client')
 
 @section('content')
-    <h1>Billing log</h1>
-    <h2>Transfers <a href="/user/client/billing/log">Payments</a></h2>
-    <table class="table">
-        <thead>
+    <div class="panel panel-default">
+        <h2 class="panel-heading">Transfers <a href="/user/client/billing/log">Payments</a></h2>
+        <table class="table panel-body booking-table">
+            
             <tr>
                 <th>#</th>
                 <th>date</th>
@@ -13,28 +13,28 @@
                 <th>banner</th>
                 <th>amount</th>
             </tr>
-        </thead>
-
-        @forelse ($transfers as $p)
-            <tr>
-                <td>{{ $p->id }}</td>
-                <td>{{ $p->created_at->format('d.m.y') }}</td>
-                @if ($p->bannerStream)
-                    <td><a href="{{ $p->bannerStream->banner->file }}">{{ $p->bannerStream->banner->title }}</a></td>
-                    <td><a href="/profile/{{ $p->bannerStream->stream->user_id }}">{{ $p->bannerStream->stream->user->name }}</a></td>
-                    <td><a href="/user/client/stream/{{ $p->bannerStream->stream_id }}">{{ $p->bannerStream->stream->time_start->format('d.m.Y') }}</a></td>
-                @else
-                    <td colspan="3"></td>
-                @endif
-                <td class="text-danger">- {{ $p->amount.' '.strtoupper($p->currency) }}</td>
-            </tr>
-        @empty
-            <tr>
-                <td colspan="6"><em>no transfers yet</em></td>
-            </tr>
-        @endforelse
-
-    </table>
+    
+            @forelse ($transfers as $p)
+                <tr>
+                    <td>{{ $p->id }}</td>
+                    <td>{{ $p->created_at->format('d.m.y') }}</td>
+                    @if ($p->bannerStream)
+                        <td><a href="{{ $p->bannerStream->banner->file }}">{{ $p->bannerStream->banner->title }}</a></td>
+                        <td><a href="/profile/{{ $p->bannerStream->stream->user_id }}">{{ $p->bannerStream->stream->user->name }}</a></td>
+                        <td><a href="/user/client/stream/{{ $p->bannerStream->stream_id }}">{{ $p->bannerStream->stream->time_start->format('d.m.Y') }}</a></td>
+                    @else
+                        <td colspan="3"></td>
+                    @endif
+                    <td class="text-danger">- {{ $p->amount.' '.strtoupper($p->currency) }}</td>
+                </tr>
+            @empty
+                <tr>
+                    <td colspan="6"><em>no transfers yet</em></td>
+                </tr>
+            @endforelse
+    
+        </table>
+    </div>
 
     {!! $transfers->render() !!}
 
