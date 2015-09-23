01ff4cc5233e0543610e05c34dc9573096d8f0ca
junkwin <s.studzinskiy@yandex.ru>
2015-09-23 16:53:49 +0300

    Update transfers.blade.php

--- resources/views/app/pages/user/twitcher/billing/transfers.blade.php
+++ resources/views/app/pages/user/twitcher/billing/transfers.blade.php
@@ -17,20 +17,20 @@
 
         @forelse ($transfers as $p)
             <tr>
-                <td>{{ $p->id }}</td>
-                <td>{{ $p->created_at->format('d.m.y') }}</td>
+                <td class="no-border">{{ $p->id }}</td>
+                <td class="no-border">{{ $p->created_at->format('d.m.y') }}</td>
                 @if ($p->bannerStream)
-                    <td><a href="{{ $p->bannerStream->banner->file }}">{{ $p->bannerStream->banner->title }}</a></td>
-                    <td><a href="/profile/{{ $p->bannerStream->banner->client_id }}">{{ $p->bannerStream->banner->client->name }}</a></td>
-                    <td><a href="/user/twitcher/stream/{{ $p->bannerStream->stream_id }}">{{ $p->bannerStream->stream->time_start->format('d.m.Y') }}</a></td>
+                    <td class="no-border"><a href="{{ $p->bannerStream->banner->file }}">{{ $p->bannerStream->banner->title }}</a></td>
+                    <td class="no-border"><a href="/profile/{{ $p->bannerStream->banner->client_id }}">{{ $p->bannerStream->banner->client->name }}</a></td>
+                    <td class="no-border"><a href="/user/twitcher/stream/{{ $p->bannerStream->stream_id }}">{{ $p->bannerStream->stream->time_start->format('d.m.Y') }}</a></td>
                 @else
-                    <td colspan="3"></td>
+                    <td class="no-border" colspan="3"></td>
                 @endif
-                <td class="text-success">+ {{ $p->amount.' '.strtoupper($p->currency) }}</td>
+                <td class="no-border" class="text-success">+ {{ $p->amount.' '.strtoupper($p->currency) }}</td>
             </tr>
         @empty
             <tr>
-                <td colspan="6"><em>no transfers yet</em></td>
+                <td class="no-border" colspan="6"><em>no transfers yet</em></td>
             </tr>
         @endforelse
 
