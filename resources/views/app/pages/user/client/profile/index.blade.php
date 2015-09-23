189af40af603dff5945c7211a4e643ebca3b514b
junkwin <s.studzinskiy@yandex.ru>
2015-09-24 00:09:01 +0300

    Update index.blade.php

--- resources/views/app/pages/user/client/profile/index.blade.php
+++ resources/views/app/pages/user/client/profile/index.blade.php
@@ -4,7 +4,7 @@
     <h1><i class="fa fa-user"></i> Profile</h1>
     {!! Form::open(['url' => '/user/client/profile/save', 'class' => 'form-horizontal']) !!}
 
-    <div class="form-group {!! ($errors && $errors->has('first_name')) ? ' has-error' : '' !!}">
+    <div class="form-group-1 {!! ($errors && $errors->has('first_name')) ? ' has-error' : '' !!}">
         {!! Form::label('first_name', 'First Name', ['class' => 'col-sm-2  control-label']) !!}
         <div class="col-sm-7">
             {!! Form::text('first_name', old('first_name', $profile->first_name), ['class' => 'form-control', 'placeholder' => 'First Name', 'required' => 'required']) !!}
@@ -12,7 +12,7 @@
         </div>
     </div>
 
-    <div class="form-group {!! ($errors && $errors->has('last_name')) ? ' has-error' : '' !!}">
+    <div class="form-group-1 {!! ($errors && $errors->has('last_name')) ? ' has-error' : '' !!}">
         {!! Form::label('last_name', 'Last Name', ['class' => 'col-sm-2  control-label']) !!}
         <div class="col-sm-7">
             {!! Form::text('last_name', old('last_name', $profile->last_name), ['class' => 'form-control', 'placeholder' => 'Last Name', 'required' => 'required']) !!}
@@ -20,7 +20,7 @@
         </div>
     </div>
 
-    <div class="form-group">
+    <div class="form-group-1">
         <div class="col-sm-offset-3 col-sm-9">
             <button type="submit" class="btn-white">Save</button>
         </div>
