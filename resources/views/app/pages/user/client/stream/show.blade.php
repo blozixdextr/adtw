7723b250bc7bd3231f086318dda175385dac0171
junkwin <s.studzinskiy@yandex.ru>
2015-09-24 01:56:08 +0300

    Update show.blade.php

--- resources/views/app/pages/user/client/stream/show.blade.php
+++ resources/views/app/pages/user/client/stream/show.blade.php
@@ -65,7 +65,7 @@
         </table>
     </div>
     <div class="panel panel-default">
-        <h3 class="panel-heading"><i class="fa fa-calendar-check-o"></i> Timelogs</h3>
+        <h3 class="panel-heading"><i class="fa fa-newspaper-o"></i> Timelogs</h3>
         <table class="table booking-table panel-body">
             <tr class="success-1">
                 <td>Time</td>
