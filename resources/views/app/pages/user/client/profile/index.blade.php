c76e652778baebd63c6ff87639b28b18d99c3038
junkwin <s.studzinskiy@yandex.ru>
2015-09-24 00:08:42 +0300

    Update index.blade.php

--- resources/views/app/pages/user/client/profile/index.blade.php
+++ resources/views/app/pages/user/client/profile/index.blade.php
@@ -4,7 +4,7 @@
     <h1><i class="fa fa-user"></i> Profile</h1>
     {!! Form::open(['url' => '/user/client/profile/save', 'class' => 'form-horizontal']) !!}
 
-    <div class="form-group-1 {!! ($errors && $errors->has('first_name')) ? ' has-error' : '' !!}">
+    <div class="form-group {!! ($errors && $errors->has('first_name')) ? ' has-error' : '' !!}">
         {!! Form::label('first_name', 'First Name', ['class' => 'col-sm-2  control-label']) !!}
         <div class="col-sm-7">
             {!! Form::text('first_name', old('first_name', $profile->first_name), ['class' => 'form-control', 'placeholder' => 'First Name', 'required' => 'required']) !!}
