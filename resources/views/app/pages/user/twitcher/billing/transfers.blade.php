8b71dc2218f57d84e030c180dffc3f0dbfd9e668
junkwin <s.studzinskiy@yandex.ru>
2015-09-23 17:07:07 +0300

    Update transfers.blade.php

--- resources/views/app/pages/user/twitcher/billing/transfers.blade.php
+++ resources/views/app/pages/user/twitcher/billing/transfers.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="panel panel-default">
     <h1 class="panel-heading">Billing log</h1>
-    <table class="table panel-body">
+    <table class="table panel-body booking-table">
         <tr>
             <th>#</th>
             <th>date</th>
