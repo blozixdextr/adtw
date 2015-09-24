4782bfddb21ac073c1294156ddd63adcd4868f02
junkwin <s.studzinskiy@yandex.ru>
2015-09-24 15:07:44 +0300

    Update left.blade.php

--- resources/views/app/includes/head/menu/twitcher/left.blade.php
+++ resources/views/app/includes/head/menu/twitcher/left.blade.php
@@ -1,7 +1,7 @@
 <div class="nav-column">
     <h2>Balance:<br> $ {{ number_format($user->balance, 2) }}</h2>
-    <h2>Dashboard</h2>
     <div class="nav-settings">
+        <h4>Dashboard</h4>
         <p class="nav-column-item"><i class="fa fa-area-chart"></i> <a href="/user/client">Timeline</a></p>
         <p class="nav-column-item"><i class="fa fa-list-alt"></i> <a href="/user/twitcher/ads">My Ads</a></p>
         <p class="nav-column-item"><i class="fa fa-money"></i> <a href="/user/twitcher/billing">Billing</a></p>
