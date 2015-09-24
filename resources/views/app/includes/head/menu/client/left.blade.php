2ea88c74da3416a28c982d6a2bfdc642af3728fb
junkwin <s.studzinskiy@yandex.ru>
2015-09-24 15:05:43 +0300

    Update left.blade.php

--- resources/views/app/includes/head/menu/client/left.blade.php
+++ resources/views/app/includes/head/menu/client/left.blade.php
@@ -1,11 +1,10 @@
 <div class="nav-column">
     <h2>Balance:<br> $ {{ number_format($user->balance, 2) }}</h2>
-    <h1>Dashboard</h1>
     <div class="nav-settings">
+        <h4>Dashboard</h4>
         <p class="nav-column-item"><i class="fa fa-area-chart"></i> <a href="/user/client">Timeline</a></p>
         <p class="nav-column-item"><i class="fa fa-search"></i> <a href="/user/client/search">Search & Buy</a></p>
         <p class="nav-column-item"><i class="fa fa-list-alt"></i> <a href="/user/client/ads">My Ads</a></p>
-
     </div>
     <div class="nav-user">
         <h4 class="nav-column-title">User</h4>
