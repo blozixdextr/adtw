d688f9814e0bf95724775982426b9eeebc555608
junkwin <s.studzinskiy@yandex.ru>
2015-09-25 19:40:27 +0300

    Update show.blade.php

--- resources/views/app/pages/user/twitcher/stream/show.blade.php
+++ resources/views/app/pages/user/twitcher/stream/show.blade.php
@@ -65,7 +65,7 @@
             </tbody>
         </table>
     </div>
-    <div class="panel panel-default">
+    <div class="panel panel-default need-scroll-little">
         <h2 class="panel-heading"><i class="fa fa-newspaper-o"></i> Timelogs</h2>
         <table class="table booking-table panel-body">
             <tr class="success-1">
