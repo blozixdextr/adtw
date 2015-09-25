127a2d1ab300749f694ef6a9e7cd5cb504ef6dec
junkwin <s.studzinskiy@yandex.ru>
2015-09-25 16:01:35 +0300

    Update index.blade.php

--- resources/views/app/pages/user/client/billing/index.blade.php
+++ resources/views/app/pages/user/client/billing/index.blade.php
@@ -9,11 +9,11 @@
 @section('content')
     <h1><i class="fa fa-university"></i> Billing</h1>
     <div class="work-column-item bil-control">
-        <div class="col-md-4"><h3>You Owe</h3><span>${{ number_format($user->balance_blocked, 2) }}</span></div>
-        <div class="col-md-4" class="work-column-link">
+        <div class="col-md-3"><h3>You Owe</h3><span>${{ number_format($user->balance_blocked, 2) }}</span></div>
+        <div class="col-md-6" class="work-column-link">
             <a class="btn-white" href="/user/client/billing/log">View Usage Details</a>
         </div>
-        <div class="col-md-4"><h3>Usage</h3><span>${{ number_format(0, 2) }}</span></div>
+        <div class="col-md-3"><h3>Usage</h3><span>${{ number_format(0, 2) }}</span></div>
     </div>
     <h1><i class="fa fa-money"></i> Payment Methods</h1>
     <div class="work-column-item" id="payments">
