45bd21b79310d7cc1ac1b00be5334d2e4bbc2b47
junkwin <s.studzinskiy@yandex.ru>
2015-09-27 13:09:39 +0300

    Update client.blade.php

--- resources/views/app/layouts/client.blade.php
+++ resources/views/app/layouts/client.blade.php
@@ -36,10 +36,10 @@
     <script type="text/javascript">
         function toggle_visibility(id) {
            var e = document.getElementById(id);
-           if(e.style.display == 'block')
-              e.style.display = 'none';
-           else
+           if(e.style.display == 'none')
               e.style.display = 'block';
+           else
+              e.style.display = 'none';
         }
     </script>
 </body>
