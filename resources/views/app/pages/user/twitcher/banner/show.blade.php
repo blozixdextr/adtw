c5dbca9e1f3355991e170da38e0285c800a8db36
junkwin <s.studzinskiy@yandex.ru>
2015-09-27 12:15:22 +0300

    Update show.blade.php

--- resources/views/app/pages/user/twitcher/banner/show.blade.php
+++ resources/views/app/pages/user/twitcher/banner/show.blade.php
@@ -26,6 +26,15 @@
             });
         });
     </script>
+    <script type="text/javascript">
+        function toggle_visibility(id) {
+           var e = document.getElementById(id);
+           if(e.style.display == 'block')
+              e.style.display = 'none';
+           else
+              e.style.display = 'block';
+        }
+    </script>
 @endsection
 
 @section('content')
