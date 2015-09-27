ea3cc662c377ebdba878f0c2575c3302e6aa8793
junkwin <s.studzinskiy@yandex.ru>
2015-09-27 13:14:30 +0300

    Update client.blade.php

--- resources/views/app/layouts/client.blade.php
+++ resources/views/app/layouts/client.blade.php
@@ -36,8 +36,8 @@
     <script type="text/javascript">
         function toggle_visibility(id) {
            var e = document.getElementById(id);
-           if(e.style.display == 'none')
-              e.style.display = 'block';
+           if(e.style.display == 'block')
+              e.style.display = 'none';
            else
               e.style.display = 'block';
         }
