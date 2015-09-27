9cd66fd78cc016ff2c364088ae90463373773c88
junkwin <s.studzinskiy@yandex.ru>
2015-09-27 12:48:17 +0300

    Update twitcher.blade.php

--- resources/views/app/layouts/twitcher.blade.php
+++ resources/views/app/layouts/twitcher.blade.php
@@ -36,8 +36,8 @@
     <script type="text/javascript">
         function toggle_visibility(id) {
            var e = document.getElementById(id);
-           if(e.style.display == 'block')
-              e.style.display = 'none';
+           if(e.style.display == 'none')
+              e.style.display = 'block';
            else
               e.style.display = 'block';
         }
