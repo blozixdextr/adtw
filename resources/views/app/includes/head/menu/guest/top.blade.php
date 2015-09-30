405f8a8f7934765e67ed05b0608b3c262908c70c
ifrond <info@ifrond.com>
2015-09-30 18:24:37 +0200

    contact us

--- resources/views/app/includes/head/menu/guest/top.blade.php
+++ resources/views/app/includes/head/menu/guest/top.blade.php
@@ -1,7 +1,7 @@
 <header id="header" class="text-center">
     <div class="shadow-fon">
-        <a href=""><img src="/assets/app/images/gold.gif"></a><br/>
-        <a href="" class="big-logo-link"></a><br>
+        <a href="/"><img src="/assets/app/images/gold.gif"></a><br/>
+        <a href="/" class="big-logo-link"></a><br>
         <span class="title-intro">Ad exchange for<br></span>
         <span class="title-descr">streamers & adverisers</span>
     </div>
