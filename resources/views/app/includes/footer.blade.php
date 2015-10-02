eb62a05f5f01e6461e47302699e60a24cbc0f0cc
blozixdextr <blozixdextr@users.noreply.github.com>
2015-10-02 10:32:09 +0200

    update mail

--- resources/views/app/includes/footer.blade.php
+++ resources/views/app/includes/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="shadow-fon">
         <ul class="icons">
 
-            <li><a href="mailto:blozixdextr@gmail.com"><i class="fa fa-envelope-o"></i> <span> Email Us Now</span></a></li>
+            <li><i class="fa fa-envelope-o"></i> <span> Email Us Now: blozixdextr@gmail.com</span></a></li>
         </ul>
         <ul class="copyright">
             <li>&copy; https://adtw.ch</li>
