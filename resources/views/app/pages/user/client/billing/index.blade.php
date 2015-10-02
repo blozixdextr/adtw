f663deaa79f411adc0ccdc6da65cac3aac0d77dc
junkwin <s.studzinskiy@yandex.ru>
2015-10-02 12:42:51 +0300

    Update index.blade.php

--- resources/views/app/pages/user/client/billing/index.blade.php
+++ resources/views/app/pages/user/client/billing/index.blade.php
@@ -18,7 +18,7 @@
         </div>
     </div>
     <h1><i class="fa fa-money"></i> Payment Methods</h1>
-    <div class="work-column-item" id="payments">
+    <div class="work-column-item fields-border" id="payments">
         <div class="tabs">
             <input id="tab1" type="radio" name="tabs" class="tab" checked="">
             <label for="tab1" title="Credit Cards" class="tab">Credit Cards</label>
