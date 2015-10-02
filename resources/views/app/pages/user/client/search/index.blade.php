2b428d34ef7c9a97e42aa9aeaff2de5700e10b47
junkwin <s.studzinskiy@yandex.ru>
2015-10-02 12:45:32 +0300

    Update index.blade.php

--- resources/views/app/pages/user/client/search/index.blade.php
+++ resources/views/app/pages/user/client/search/index.blade.php
@@ -20,7 +20,7 @@ if (isset($filters['banner_types']) && count($filters['banner_types']) > 0) {
 @endsection
 @section('content')
     
-    <div class="panel panel-default choose-strimer">
+    <div class="panel panel-default choose-strimer fields-border">
         <div class="panel-heading"><h2><i class="fa fa-filter"></i> Streamers Filter</h2></div>
         <div class="panel-body">
             {!! Form::open(['url' => '/user/client/search', 'class' => '', 'method' => 'get']) !!}
