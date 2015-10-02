6468bbf4d88ff61ee990733c2bfb5cd9fb944590
blozixdextr <blozixdextr@users.noreply.github.com>
2015-10-02 10:29:47 +0200

    update

--- resources/views/app/includes/footer.blade.php
+++ resources/views/app/includes/footer.blade.php
@@ -1,12 +1,11 @@
 <footer id="footer">
     <div class="shadow-fon">
         <ul class="icons">
-            <li><a href="#"><i class="fa fa-facebook"></i> <span> Facebook</span></a></li>
-            <li><a href="#"><i class="fa fa-twitter"></i> <span> Twitter</span></a></li>
-            <li><a href="#"><i class="fa fa-envelope-o"></i> <span> Email</span></a></li>
+
+            <li><a href="mailto:blozixdextr@gmail.com"><i class="fa fa-envelope-o"></i> <span> Email Us Now</span></a></li>
         </ul>
         <ul class="copyright">
-            <li>&copy; www.adtw.ch</li>
+            <li>&copy; https://adtw.ch</li>
         </ul>
     </div>
 </footer>
