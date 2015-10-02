76e6f931e78ce7f6eb9f30c9e16533b73d8c38c6
junkwin <s.studzinskiy@yandex.ru>
2015-10-02 13:50:42 +0300

    Update index.blade.php

--- resources/views/app/pages/user/twitcher/billing/index.blade.php
+++ resources/views/app/pages/user/twitcher/billing/index.blade.php
@@ -56,7 +56,7 @@
         </div>
         <div class="form-group">
             <div class="col-sm-offset-3 col-sm-9">
-                <button type="submit" class="btn-white">Withdraw</button>
+                <button type="submit" class="need-mt-30 btn-white">Withdraw</button>
             </div>
         </div>
         {!! Form::close() !!}
