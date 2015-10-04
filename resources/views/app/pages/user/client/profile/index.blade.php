65c318aa6f2ce3097ce3bd292842daa7dfcd6c5c
junkwin <s.studzinskiy@yandex.ru>
2015-10-04 15:11:56 +0300

    Update index.blade.php

--- resources/views/app/pages/user/client/profile/index.blade.php
+++ resources/views/app/pages/user/client/profile/index.blade.php
@@ -1,35 +1,8 @@
 @extends('app.layouts.client')
 
 @section('content')
-    <h1><i class="fa fa-user"></i> Profile</h1>
-    {!! Form::open(['url' => '/user/client/profile/save', 'class' => 'form-horizontal profile-table']) !!}
-    <div class="fields-border">
-    <div class="form-group-1 width-50 {!! ($errors && $errors->has('first_name')) ? ' has-error' : '' !!}">
-        {!! Form::label('first_name', 'First Name', ['class' => 'control-label']) !!}
-        <div>
-            {!! Form::text('first_name', old('first_name', $profile->first_name), ['class' => 'form-control', 'placeholder' => 'First Name', 'required' => 'required']) !!}
-            {!! Form::errorMessage('first_name') !!}
-        </div>
-    </div>
 
-    <div class="form-group-1 width-50 {!! ($errors && $errors->has('last_name')) ? ' has-error' : '' !!}">
-        {!! Form::label('last_name', 'Last Name', ['class' => 'control-label']) !!}
-        <div>
-            {!! Form::text('last_name', old('last_name', $profile->last_name), ['class' => 'form-control', 'placeholder' => 'Last Name', 'required' => 'required']) !!}
-            {!! Form::errorMessage('last_name') !!}
-        </div>
-    </div>
-
-    <div class="form-group-1 width-50 text-center">
-        <div>
-            <button type="submit" class="btn-white">Save</button>
-        </div>
-    </div>
-    </div>
-    {!! Form::close() !!}
-
-
-    <h2><i class="fa fa-key"></i> Change Password</h2>
+    <h1><i class="fa fa-key"></i> Change Password</h1>
     {!! Form::open(['url' => '/user/client/profile/password', 'class' => 'form-horizontal profile-table']) !!}
     <div class="fields-border">
     <div class="form-group-1 width-50 {!! ($errors && $errors->has('password')) ? ' has-error' : '' !!}">
@@ -53,6 +26,33 @@
         <div>
             {!! Form::password('new_password2', ['class' => 'form-control', 'placeholder' => 'Confirm New Password', 'required' => 'required']) !!}
             {!! Form::errorMessage('new_password2') !!}
+        </div>
+    </div>
+
+    <div class="form-group-1 width-50 text-center">
+        <div>
+            <button type="submit" class="btn-white">Save</button>
+        </div>
+    </div>
+    </div>
+    {!! Form::close() !!}
+
+    <h1><i class="fa fa-user"></i> Profile</h1>
+    {!! Form::open(['url' => '/user/client/profile/save', 'class' => 'form-horizontal profile-table']) !!}
+    <div class="fields-border">
+    <div class="form-group-1 width-50 {!! ($errors && $errors->has('first_name')) ? ' has-error' : '' !!}">
+        {!! Form::label('first_name', 'First Name', ['class' => 'control-label']) !!}
+        <div>
+            {!! Form::text('first_name', old('first_name', $profile->first_name), ['class' => 'form-control', 'placeholder' => 'First Name', 'required' => 'required']) !!}
+            {!! Form::errorMessage('first_name') !!}
+        </div>
+    </div>
+
+    <div class="form-group-1 width-50 {!! ($errors && $errors->has('last_name')) ? ' has-error' : '' !!}">
+        {!! Form::label('last_name', 'Last Name', ['class' => 'control-label']) !!}
+        <div>
+            {!! Form::text('last_name', old('last_name', $profile->last_name), ['class' => 'form-control', 'placeholder' => 'Last Name', 'required' => 'required']) !!}
+            {!! Form::errorMessage('last_name') !!}
         </div>
     </div>
 
