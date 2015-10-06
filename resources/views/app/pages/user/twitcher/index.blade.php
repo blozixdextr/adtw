537a56f7c1fed4c7ac18dd91313cc48f13688d2d
junkwin <s.studzinskiy@yandex.ru>
2015-10-06 16:47:17 +0300

    Update index.blade.php

--- resources/views/app/pages/user/twitcher/index.blade.php
+++ resources/views/app/pages/user/twitcher/index.blade.php
@@ -50,7 +50,7 @@
     <div class="panel booking-table-first panel-default">
         <h2 class="panel-heading"><i class="fa fa-play-circle"></i> Banners ready to start</h2>
         <div class="panel-body booking-table">
-            <div class="col-xs-6">
+            <div class="col-xs-5">
         @if (count($activeBanners) > 0)
             @forelse($bannerTypes as $bt)
                 @if (isset($banners[$bt->id]) && count($banners[$bt->id]) > 0)
