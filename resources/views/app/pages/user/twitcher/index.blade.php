e3f71649d27abae2ebcd55f6ce58dd445a4e720f
junkwin <s.studzinskiy@yandex.ru>
2015-10-06 16:25:33 +0300

    Update index.blade.php

--- resources/views/app/pages/user/twitcher/index.blade.php
+++ resources/views/app/pages/user/twitcher/index.blade.php
@@ -76,10 +76,8 @@
             </div>
             <div class="col-xs-6">
                 <div>
-                    <div>
-                        <p><strong>If you want to start with all banners, click here</strong></p>
-                    </div>
-                    <a href="#" id="startPopups" class="btn-white">Start all together</a>
+                    <p><strong>If you want to start with all banners, click here</strong></p>
+                    <div><a href="#" id="startPopups" class="btn-white">Start all together</a></div>
                 </div>
                 <div>
                     <p><strong>Background color for banners</strong></p>
