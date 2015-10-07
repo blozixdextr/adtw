80f94d2840b1bfaff3df2ebc8744374fe083fc62
junkwin <s.studzinskiy@yandex.ru>
2015-10-07 13:50:29 +0300

    Update index.blade.php

--- resources/views/app/pages/user/twitcher/index.blade.php
+++ resources/views/app/pages/user/twitcher/index.blade.php
@@ -50,7 +50,7 @@
     <div class="panel booking-table-first panel-default ready-for-stream">
         <h2 class="panel-heading"><i class="fa fa-play-circle"></i> Banners ready to start</h2>
         <div class="panel-body booking-table">
-            <div class="col-lg-6 col-md-12 new-flex together_color">
+            <div class="col-lg-6 col-md-12 col-sm-6 new-flex together_color">
                 <div class="col-lg-12 col-md-6 col-lg-offset-0 col-md-offset-3 new-flex">
                     <div class="col-lg-5 col-md-6 input-group" id="bgColorPicker">
                         <input type="text" value="#000000" class="form-control thin-border"  id="bgColor" />
@@ -58,7 +58,7 @@
                     </div>
                     <p class="col-lg-6 col-md-6 col-lg-offset-1"><i>Set up background color for banners</i></p>
                 </div>
-                <div class="col-lg-12 col-md-12">
+                <div class="col-lg-12 col-sm-6 col-md-12">
                     <div class="start-together">
                         <a href="#" id="startPopups" class="btn-white">START STREAM</a>
                     </div>
