f4e99abdb2ea2e2fb847a7e792a90001f765cdfa
junkwin <s.studzinskiy@yandex.ru>
2015-10-07 12:22:10 +0300

    Update index.blade.php

--- resources/views/app/pages/user/twitcher/index.blade.php
+++ resources/views/app/pages/user/twitcher/index.blade.php
@@ -92,7 +92,7 @@
             </div>
             
         @else
-            <div class="col-xs-8 col-xs-offset-4 ready-banner">
+            <div class="col-xs-6 col-xs-offset-3 ready-banner">
                 <strong>There are no orders yet</strong>
             </div>
         @endif
